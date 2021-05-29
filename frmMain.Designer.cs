0ed4585e27e2299016f99f901ce2cac34767b4a8
Kenneth Lin <kennethlin88@gmail.com>
2021-05-29 12:40:15 -0500

    Add project files.

---
+++ frmMain.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace Stock_YahooFinance
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstTickers = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lstTickers
+            // 
+            this.lstTickers.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstTickers.FormattingEnabled = true;
+            this.lstTickers.ItemHeight = 25;
+            this.lstTickers.Location = new System.Drawing.Point(36, 37);
+            this.lstTickers.Name = "lstTickers";
+            this.lstTickers.Size = new System.Drawing.Size(384, 429);
+            this.lstTickers.TabIndex = 0;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(902, 523);
+            this.Controls.Add(this.lstTickers);
+            this.Name = "frmMain";
+            this.Text = "Main Form";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstTickers;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using YahooFinanceApi;
+
+namespace Stock_YahooFinance
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            GetPrice();
+        }
+
+        private static async void GetPrice()
+        {
+            var securities = await Yahoo.Symbols("AAPL", "GOOG").Fields(Field.Symbol, Field.RegularMarketPrice, 
+                        Field.FiftyTwoWeekHigh, Field.ShortName, Field.FiftyDayAverage).QueryAsync();
+            var aapl = securities["AAPL"];
+            var price = aapl[Field.FiftyDayAverage];
+
+
+            var historyData = await Yahoo.GetHistoricalAsync("AAPL", new DateTime(2021, 5, 1),
+                new DateTime(2021, 5, 28), Period.Daily);
+
+            var data1 = historyData[0];
+
+
+            MessageBox.Show("hi");
+
+        }
+    }
+}
